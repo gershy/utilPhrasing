31e693e938081dc1036ecb336920b25e4f25e1b3
Gershom Maes <gershommaes@protonmail.com>
2026-03-07 18:37:24 -0500

    <automated>

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,13 +1,23 @@
 import { assertEqual } from '../build/utils.test.ts';
-import './main.ts';
+import phrasing, { Mode } from './main.ts';
+
+// Note we're 
+const allStrModes = [ 'camel', 'kebab', 'snake', 'kamel' ] as const;
+type AllStrModes = typeof allStrModes;
+type StrMode = AllStrModes[number];
 
 // Type testing
 (async () => {
   
   type Enforce<Provided, Expected extends Provided> = { provided: Provided, expected: Expected };
   
+  const arr = phrasing('abc', 'camel', 'parts');
+  const str = phrasing('abc', 'camel', allStrModes[0 as number]);
+  
   type Tests = {
-    1: Enforce<{ x: 'y' }, { x: 'y' }>,
+    1: Enforce<typeof arr, string[]>,
+    2: Enforce<typeof str, string>,
+    3: Enforce<StrMode, Mode>, // Note this test forces `allStrModes` in this file to include all modes from `main.ts`!
   };
   
 })();
@@ -17,14 +27,36 @@ import './main.ts';
   
   const cases = [
     
-    {
-      name: 'not implemented',
-      fn: async () => {
-        
-        // TODO: Implement!
-        
-      }
-    }
+    { name: 'basic test', fn: async () => {
+      
+      assertEqual(
+        phrasing('testyMan', 'camel', 'camel'),
+        'testyMan'
+      );
+      assertEqual(
+        phrasing('testyMan', 'camel', 'kebab'),
+        'testy-man'
+      );
+      assertEqual(
+        phrasing('testyMan', 'camel', 'snake'),
+        'testy_man'
+      );
+      assertEqual(
+        phrasing('testyMan', 'camel', 'kamel'),
+        'TestyMan'
+      );
+      
+    }},
+    { name: 'fuzz', fn: async () => {
+      
+      for (const src of allStrModes)
+        for (const trg of allStrModes)
+          assertEqual(phrasing('', src, trg), '');
+      
+      for (const src of allStrModes)
+        assertEqual(phrasing('', src, 'parts'), []);
+      
+    }}
     
   ];
   for (const { name, fn } of cases) {
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
-import '@gershy/clearing';
+export type Mode = 'camel' | 'kebab' | 'snake' | 'kamel';
 
-export default null;
+export default ((str: string, src: Mode, trg: 'parts' | Mode) => {
+  
+  const splitReg = { camel: /(?=[A-Z])/, kamel: /(?=[A-Z])/, kebab: /[-]+/, snake: /[_]+/ }[src];
+  const parts = str.split(splitReg).map(cmp => cmp[lower]()).filter(v => !!v);
+  
+  if (trg === 'parts') return parts;
+  if (trg === 'camel') return [ parts[0], ...parts.slice(1).map(v => v[0][upper]() + v.slice(1)) ].join('');
+  if (trg === 'kamel') return parts                        .map(v => v[0][upper]() + v.slice(1))  .join('');
+  if (trg === 'kebab') return parts.join('-');
+  if (trg === 'snake') return parts.join('_');
+  
+  throw Error('ouch');
+  
+}) as ({
+  
+  (str: string, src: Mode, trg: 'parts'): string[],
+  (str: string, src: Mode, trg: Mode):    string,
+  
+});
